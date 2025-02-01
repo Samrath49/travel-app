ce20b9232cb1efe7d9c012a500f3dcf0493e786a
samrath49 <samrathchauhan49@gmail.com>
2025-02-02 01:09:14 +0530

    feat: Assets + minor changes

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1 class="text-3xl text-fuchsia-400">Hi there this is header</h1>
+  <h1 class="text-4xl font-volkhov font-bold mb-4">Welcome to My Site</h1>
+  <p class="font-inter text-lg">This text uses Inter font family</p>
+  <p class="font-poppins text-lg">This text uses Poppins font family</p>
+  <p class="font-roboto text-lg">This text uses Roboto font family</p>
 </template>
---
+++ tailwind.config.ts
@@ -0,0 +1,46 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        volkhov: ["Volkhov", "serif"],
+      },
+      colors: {
+        primary: "#FA7435",
+        "bg-main": "#FEFCFB",
+        "bg-special": "#FDFCFA",
+        "bg-alt": "#F7F8FC",
+        "star-yellow": "rgb(245, 245, 19)",
+        "gray-light": "rgba(151, 151, 147, 0.616)",
+        "gray-medium": "rgb(202, 200, 200)",
+      },
+      spacing: {
+        22: "5.5rem",
+        25: "6.25rem",
+        30: "7.5rem",
+      },
+      borderRadius: {
+        "1.5vh": "1.5vh",
+      },
+      boxShadow: {
+        floating: "0px 2px 80px 2px rgba(0, 0, 0, 0.06)",
+      },
+      backgroundImage: {
+        "card-signup": "url('../img/Vector4857.png')",
+        "card-exciting": "url('../img/Vector4857.png')",
+        "card-worth": "url('../img/Vector4856.png')",
+      },
+    },
+  },
+} satisfies Config;
