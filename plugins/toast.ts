2d97b0341266f1d0e15b704f9fd3be2cbd77b190
samrath49 <samrathchauhan49@gmail.com>
2025-02-05 16:16:45 +0530

    feat: Plugin for using toast.

---
+++ plugins/toast.ts
@@ -0,0 +1,24 @@
+import Toast, { POSITION, type PluginOptions } from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const options: PluginOptions = {
+    position: "bottom-right" as POSITION,
+    timeout: 4000,
+    closeOnClick: false,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: true,
+    hideProgressBar: false,
+    closeButton: "button",
+    icon: true,
+    rtl: false,
+    transition: "Vue-Toastification__fade",
+    maxToasts: 20,
+    newestOnTop: true,
+  };
+
+  nuxtApp.vueApp.use(Toast, options);
+});
